168a23b445493eb1cfc6b4510d6ef28e1de17203
KwonYiJun <15031795@myrp.edu.sg>
2017-04-25 14:49:22 +0800

    Im fat

--- app/src/main/java/com/myapplicationdev/android/tw_listview/FirstActivity.java
+++ app/src/main/java/com/myapplicationdev/android/tw_listview/FirstActivity.java
@@ -1,7 +1,10 @@
 package com.myapplicationdev.android.tw_listview;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -22,12 +25,24 @@ public class FirstActivity extends AppCompatActivity {
 
         lv = (ListView)findViewById(R. id. lvYear);
         tv = (TextView)findViewById(R. id. textview);
+        aa = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, al);
 
         al = new ArrayList<String>();
         al.add("Year 1");
         al.add("Year 2");
         al.add("Year 3");
 
+        lv.setAdapter(aa);
+
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String year = al.get(position).toString();
+                Intent i = new Intent(FirstActivity.this, SecondActivity.class);
+                startActivity(i);
+            }
+        });
 
     }
 }
